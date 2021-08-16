4cc2f53e29ff357c24a124e7c3871a3a8a4d931b
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-08-16 10:41:30 -0300

    Setup do projeto

---
+++ src/main/java/com/github/zlbovolini/proposta/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.zlbovolini.proposta.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String say() {
+        return "Hello world";
+    }
+}
