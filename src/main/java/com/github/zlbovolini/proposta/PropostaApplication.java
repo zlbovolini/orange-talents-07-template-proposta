8b093ada146f9560c51416c354e9bf6d183422a7
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-08-17 17:48:27 -0300

    Desabilitado comportamento transacional de metodos de Repository

--- src/main/java/com/github/zlbovolini/proposta/PropostaApplication.java
+++ src/main/java/com/github/zlbovolini/proposta/PropostaApplication.java
@@ -3,9 +3,11 @@ package com.github.zlbovolini.proposta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class PropostaApplication {
 
 	public static void main(String[] args) {
