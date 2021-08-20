9dd89b0b7f736f04c2d55d2e32b09e4ee3861a9f
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-08-20 10:32:12 -0300

    Adicionado autenticacao e autorizacao

---
+++ src/main/java/com/github/zlbovolini/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.github.zlbovolini.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .antMatchers(HttpMethod.GET, "/api/v1/propostas/**")
+                        .hasAuthority("SCOPE_propostas")
+                    .antMatchers(HttpMethod.GET, "/api/v1/cartoes/**")
+                        .hasAuthority("SCOPE_cartoes")
+                    .antMatchers(HttpMethod.POST, "/api/v1/propostas/**")
+                        .hasAuthority("SCOPE_propostas")
+                    .antMatchers(HttpMethod.POST, "/api/v1/cartoes/**")
+                        .hasAuthority("SCOPE_cartoes")
+                    .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .csrf().disable();
+    }
+}
